de1b0a2eb7562652d066b9263cf3fc8bf9bb832a
Fabian Rangel <rangel_frc@hotmail.com>
2020-06-17 16:19:06 -0500

    Blinked D1 example and hardware change list added.

---
+++ Firmware/Example/blink.c
@@ -0,0 +1,25 @@
+#include <compiler.h>
+#include <stdint.h>
+
+SFR(PORT_CFG,	0xC6); // Port Configuration Register
+SFR(P3_DIR,	0xBE);	// P3 port direction control register
+SFR(P3,	0xB0);	// P3 port input and output register
+
+SBIT(LED2, 0xB0, 2); // accessing pin 3.2
+
+static inline void delay() {
+    uint32_t i;
+    for (i = 0; i < (120000UL); i++){}
+        __asm__("nop");
+}
+
+void main() {
+	PORT_CFG = 0b00101001;
+    P3_DIR = 0b00001111;
+	P3 = 0x00;
+
+	while (1) {
+		delay();
+		LED2 = !LED2;
+	}
+}
---
+++ Hardware/CAMBIOS DE HARDWARE CH559 WINGHUB.md
@@ -0,0 +1,6 @@
+# CAMBIOS DE HARDWARE CH559 WINGHUB
+En esta sección se enlistará los cambios de hardware que se implementarán en la PCB.
+
+- Cambiar la posición de los botones hacia la orilla.
+- Cambiar la posición del LED hacia la orilla opuesta al usb conector.
+- Anotar el pin de 3.3 en el silkscreen.
